e868edc6e2e58929ad44fe58a9dae95210c65137
Andrew Broman <broman@acm.org>
2016-03-04 19:21:14 -0600

    Implement fetch_weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = '9e028a8eb9c5c4e0f4a8ef7b0e2cc7e1'
+import axios from 'axios';
+
+const API_KEY = '9e028a8eb9c5c4e0f4a8ef7b0e2cc7e1';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export constant FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
